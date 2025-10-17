9b4cd22cbf71277bb79706c86abeabb3e5955fcd
Sa7yaC <131731688+Sa7yaC@users.noreply.github.com>
2025-10-17 14:00:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 from PIL import Image
 import io, json, os
 import numpy as np
-# import tensorflow as tf
-# from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
+import tensorflow as tf
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
 
 st.set_page_config(page_title="Smart Recipe Generator", layout="wide")
 
@@ -146,5 +146,6 @@ if st.button("Generate Recipes"):
 
 st.markdown("---")
 st.caption("Built with ❤️ using Streamlit & TensorFlow | Smart Recipe Generator Demo")
+
 
 
